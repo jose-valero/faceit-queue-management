778c59d3c41d2b9b21c07e5a77b2a1b824be86ed
Juan Valero <ryesaju@amazon.com>
2025-09-16 20:20:39 -0700

    script to deploy into ECR

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Get current commit hash
+COMMIT_HASH=$(git rev-parse --short HEAD)
+ECR_REPO="506636091874.dkr.ecr.us-east-1.amazonaws.com/dev-faceit-cluster-app"
+
+# Build Go app for ARM64
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o app .
+
+# Login to ECR
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REPO
+
+# Build Docker image using ECS Dockerfile
+docker build -f Dockerfile-ecs -t $ECR_REPO:$COMMIT_HASH .
+
+# Push to ECR
+docker push $ECR_REPO:$COMMIT_HASH
+
+# Clean up binary
+rm app
+
+echo "Pushed image: $ECR_REPO:$COMMIT_HASH"
