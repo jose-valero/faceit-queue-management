ffc77a9295f3e0785cd8de7995a43b10e4bd4fb6
Juan Valero <ryesaju@amazon.com>
2025-09-16 21:47:10 -0700

    small fixes for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,16 @@ COMMIT_HASH=$(git rev-parse --short HEAD)
 ECR_REPO="506636091874.dkr.ecr.us-east-1.amazonaws.com/dev-faceit-cluster-app"
 
 # Build Go app for ARM64
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o app .
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o app ./cmd/bot
 
 # Login to ECR
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REPO
+aws ecr get-login-password --region us-east-1 --profile cpx-valero | docker login --username AWS --password-stdin $ECR_REPO
 
 # Build Docker image using ECS Dockerfile
 docker build -f Dockerfile-ecs -t $ECR_REPO:$COMMIT_HASH .
 
 # Push to ECR
-docker push $ECR_REPO:$COMMIT_HASH
+AWS_PROFILE=cpx-valero docker push $ECR_REPO:$COMMIT_HASH
 
 # Clean up binary
 rm app
